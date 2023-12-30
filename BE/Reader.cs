df7a64c4cce7aa7d24c5f8b760465c206d9ab984
Donnie Sunset <Donnie.Sunset@gmail.com>
2023-12-30 19:58:11 +0100

    Display a single hardcoded cover image from a zipped epub

---
+++ BE/Reader.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace BE
+{
+    public class Reader
+    {
+
+        public MemoryStream GetImage()
+        {
+            string ebookFolder = @"P:\Ebooks\Romane";
+            string fileName = @"C:\temp\Steinbeck, John - 1932 - Eine Handvoll Gold.epub";
+
+            //foreach (var fileName in Directory.GetFiles(ebookFolder, "*", SearchOption.AllDirectories))
+            //{
+
+            using var zipFile = ZipFile.OpenRead(fileName);
+
+            var jpgFile = zipFile.Entries.SingleOrDefault(x => x.Name.EndsWith("cover.jpg", StringComparison.CurrentCultureIgnoreCase) || x.Name.EndsWith("cover.jpeg", StringComparison.CurrentCultureIgnoreCase));
+
+            MemoryStream decompressedMemoryStream = new MemoryStream();
+
+            if (jpgFile != null)
+            {
+                using (Stream jpgDeflateStream = jpgFile.Open())
+                {
+
+                    MemoryStream memoryStream = new MemoryStream();
+                    jpgDeflateStream.CopyTo(memoryStream);
+
+                    return memoryStream;
+
+                }
+            }
+            else
+            {
+                throw new Exception("something went wrong.");
+            }
+
+            //}
+        }
+    }
+}
--- Gui/ViewModels/MainViewModel.cs
+++ Gui/ViewModels/MainViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections.ObjectModel;
+﻿using BE;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Runtime.CompilerServices;
 
 namespace Gui.ViewModels
 {
     public class MainViewModel : BindableObject, INotifyPropertyChanged
     {
+        public class ImageItem
+        {
+            public string ImagePath { get; set; }
+            public StreamImageSource Source { get; set; }
+        }
+
         private ObservableCollection<ImageItem> _imageItems;
+        private Reader reader = new Reader();
 
         public ObservableCollection<ImageItem> ImageItems
         {
@@ -20,16 +29,33 @@ namespace Gui.ViewModels
 
         public MainViewModel()
         {
+            MemoryStream memoryStream = reader.GetImage();
+            MemoryStream memoryStream2 = reader.GetImage();
+
+            StreamImageSource streamImageSource = (StreamImageSource)StreamImageSource.FromStream(() =>
+            {
+                memoryStream.Seek(0, SeekOrigin.Begin); // Ensure the MemoryStream is at the beginning
+                return memoryStream;
+            });
+
+            StreamImageSource streamImageSource2 = (StreamImageSource)StreamImageSource.FromStream(() =>
+            {
+                memoryStream2.Seek(0, SeekOrigin.Begin); // Ensure the MemoryStream is at the beginning
+                return memoryStream2;
+            });
+
+            //StreamImageSource imageSource = (StreamImageSource)ImageSource.FromStream(() => { return bla; });
+
             // Hardcoded image paths, replace these with your actual image paths
             ImageItems = new ObservableCollection<ImageItem>
             {
-                new ImageItem { ImagePath = @"P:\Bilder\2011-02-14 09.29.43.jpg" },
-                new ImageItem { ImagePath = @"P:\Bilder\2011-02-14 09.29.43.jpg" },
-                new ImageItem { ImagePath = @"P:\Bilder\2011-02-14 09.29.43.jpg" },
-                new ImageItem { ImagePath = @"P:\Bilder\2011-02-14 09.29.43.jpg" },
-                new ImageItem { ImagePath = @"P:\Bilder\2011-02-14 09.29.43.jpg" },
-                new ImageItem { ImagePath = @"P:\Bilder\2011-02-14 09.29.43.jpg" },
-                new ImageItem { ImagePath = @"P:\Bilder\2011-02-14 09.29.43.jpg" },
+                new ImageItem { ImagePath = @"P:\Bilder\2011-02-14 09.29.43.jpg", Source = streamImageSource },
+                new ImageItem { ImagePath = @"P:\Bilder\2011-02-14 09.29.43.jpg", Source = streamImageSource2 },
+                //new ImageItem { ImagePath = @"P:\Bilder\2011-02-14 09.29.43.jpg", Source = streamImageSource },
+                //new ImageItem { ImagePath = @"P:\Bilder\2011-02-14 09.29.43.jpg", Source = streamImageSource },
+                //new ImageItem { ImagePath = @"P:\Bilder\2011-02-14 09.29.43.jpg", Source = streamImageSource },
+                //new ImageItem { ImagePath = @"P:\Bilder\2011-02-14 09.29.43.jpg", Source = streamImageSource },
+                //new ImageItem { ImagePath = @"P:\Bilder\2011-02-14 09.29.43.jpg", Source = streamImageSource },
                 // Add more image paths as needed
 
             };
@@ -43,8 +69,5 @@ namespace Gui.ViewModels
         }
     }
 
-    public class ImageItem
-    {
-        public string ImagePath { get; set; }
-    }
+
 }
